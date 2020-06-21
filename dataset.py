e7f9fb7b6416abb28f022b41a141512ec3756da8
Rico Suave <49012231+RicoSuaveGuapo@users.noreply.github.com>
2020-06-21 20:48:57 +0800

    train_val_test dataset class-depend distribution done

--- dataset.py
+++ dataset.py
@@ -7,6 +7,7 @@ from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 
 from transform import get_transfrom, test_transfrom
+from utils import train_val_test_index
 
 class MetalDataset(Dataset):
 
@@ -15,48 +16,43 @@ class MetalDataset(Dataset):
         assert mode in ['train', 'test', 'val']
         super().__init__()
 
-        assert os.getcwd() == '/home/rico-li/Job/Metal', 'in the wrong working directory'
+        # assert os.getcwd() == '/home/rico-li/Job/Metal', 'in the wrong working directory'
         path = os.getcwd()+'/Image'
         class_names = os.listdir(path)
         class_names.sort()
         
-        label_i = 0
         label = []
         image_path = []
-        for class_name in class_names:
+        for i, class_name in enumerate(class_names):
             image_names = os.listdir(f'{path}/{class_name}')
             image_path += [f'{path}/{class_name}/{image_name}' for image_name in image_names]
-            label += [label_i] * len(image_names)
-            label_i += 1
-
-        self.data_path = image_path
-        self.label = label
-
+            label += [i] * len(image_names)
+            
         self.mode = mode
         self.transform = transform
         self.image_size = image_size
 
-        train = mode in ['train', 'val']
-
-        np.random.seed(seed)
-        index_list = list(range(len(self.label)))
-
+        # train = mode in ['train', 'val']
         # TODO: should include class distribution into the train/val/test set.
-        index_list = np.random.permutation(index_list)
+        # index_list = list(range(len(label)))
+        # index_list = np.random.permutation(index_list)
 
-        if train:
-            split_1 = int(len(self.label) * (1 - val_split - test_spilt))
-            split_2 = int(len(self.label) * (1 - test_spilt))
+        # if train:
+        #     split_1 = int(len(self.label) * (1 - val_split - test_spilt))
+        #     split_2 = int(len(self.label) * (1 - test_spilt))
 
-            if self.mode == 'train':
-                self.index_list = index_list[:split_1]
-            elif self.mode == 'val':
-                self.index_list = index_list[split_1:split_2]
-        else:
-            split_2 = int(len(self.label) * (1 - test_spilt))
-            self.index_list = index_list[split_2:]
-
-
+        #     if self.mode == 'train':
+        #         self.index_list = index_list[:split_1]
+        #     elif self.mode == 'val':
+        #         self.index_list = index_list[split_1:split_2]
+        # else:
+        #     split_2 = int(len(self.label) * (1 - test_spilt))
+        #     self.index_list = index_list[split_2:]
+        np.random.seed(seed)
+        self.index_list = train_val_test_index(label, mode, val_split, test_spilt)
+        self.data_path = [image_path[i] for i in self.index_list]
+        self.label = [label[i] for i in self.index_list] 
+            
     def __len__(self):
         return len(self.index_list)
 
@@ -74,9 +70,26 @@ class MetalDataset(Dataset):
         return image, label
 
 if __name__ == '__main__':
-    dataset = MetalDataset(mode='val', transform=True)
+    dataset = MetalDataset(mode='train', transform=True)
     dataloader = DataLoader(dataset, batch_size=10, shuffle=True)
     for image, label in dataloader:
         print(image.shape)
         print(label)
         break
+
+    # a = [0]*5 + [1]*4
+    # a = np.array(a)
+    # a_uni = np.unique(a)
+    # train_index = []
+    # val_index = []
+    # test_index = []
+
+    # for i in a_uni:
+    #     index = np.where(a == i)
+    #     class_index = np.random.permutation(index[0]).tolist()
+    #     train_index += class_index[:round(len(class_index)*0.6)]
+    #     val_index += class_index[round(len(class_index)*0.6): round(len(class_index)*0.9)]
+    #     test_index += class_index[round(len(class_index)*0.9):]
+    #     print(train_index, val_index, test_index)
+
+    
